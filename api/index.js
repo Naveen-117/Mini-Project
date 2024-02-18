ce8d0ff09b85fef79cffe22295ce3edd544cee23
Naveen-117 <naveenantonyp@gmail.com>
2024-02-18 20:07:21 +0530

    created middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://naveenantonyp:mech1032@cluster0.palocap.mongodb.net/property-website?retryWrites=true&w=majority").then(() => {
     console.log('Connected to MongoDB!');
@@ -11,9 +12,25 @@ mongoose.connect("mongodb+srv://naveenantonyp:mech1032@cluster0.palocap.mongodb.
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 
 app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    }
+
+    );
+});
